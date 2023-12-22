cf5e84ee3d84af73153e06eb0e61fd5d5517eb90
Touta AS <20009@esp.mr>
2023-12-22 15:14:39 +0000

    payment commit v2

---
+++ payment/src/main/java/com/enit/payment/services/BankService.java
@@ -0,0 +1,23 @@
+package com.enit.payment.services;
+
+
+import com.enit.payment.dto.PaymentRequest;
+import feign.Response;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.math.BigDecimal;
+
+@FeignClient(name = "bank-service", url = "http://localhost:8099/bank")
+public interface BankService {
+    @GetMapping("/{id}")
+    BigDecimal withdrawMoneyFromAccount(@RequestParam("money") BigDecimal money, @PathVariable("id") Long id);
+
+    @PostMapping
+    public Response makeNewPayment(PaymentRequest cmd);
+
+
+}
