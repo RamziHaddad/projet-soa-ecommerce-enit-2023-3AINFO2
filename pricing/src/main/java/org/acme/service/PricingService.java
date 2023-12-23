e7af92ae544174c65582a80ed751e90c33207f59
farah-abid <92029907+farah-abid@users.noreply.github.com>
2023-12-23 10:00:40 +0100

    Update PricingService.java

--- pricing/src/main/java/org/acme/service/PricingService.java
+++ pricing/src/main/java/org/acme/service/PricingService.java
@@ -1,5 +1,4 @@
 
-/*
 package org.acme.service;
 
 import org.acme.api.dtos.OrderPricingDTO;
@@ -8,48 +7,37 @@ import java.math.BigDecimal;
 import java.util.Map;
 import java.util.UUID;
 
-
 public interface ProductPricingStrategy {
     BigDecimal calculateProductPrice(UUID productId, int quantity);
 }
 
-//simple fixed price strategy
 class FixedPriceStrategy implements ProductPricingStrategy {
     private static final BigDecimal PRODUCT_PRICE = new BigDecimal("10.0");
 
     @Override
     public BigDecimal calculateProductPrice(UUID productId, int quantity) {
-        
         return PRODUCT_PRICE.multiply(BigDecimal.valueOf(quantity));
     }
 }
 
-//interface with customizable pricing strategy
 public class CustomPricingService implements PricingService {
 
     private final ProductPricingStrategy pricingStrategy;
 
-    // injecting the pricing strategy during instantiation
     public CustomPricingService(ProductPricingStrategy pricingStrategy) {
         this.pricingStrategy = pricingStrategy;
     }
 
     @Override
     public BigDecimal priceProducts(OrderPricingDTO orderPricingDTO) {
-        // Extract info from  DTO
         Map<UUID, Integer> productMap = orderPricingDTO.productMap();
-
-        // total price based on provided pricing strategy
         BigDecimal totalPrice = BigDecimal.ZERO;
         for (Map.Entry<UUID, Integer> entry : productMap.entrySet()) {
             UUID productId = entry.getKey();
             int quantity = entry.getValue();
-            // Use the pricing strategy to calculate the price for each product
             BigDecimal productTotal = pricingStrategy.calculateProductPrice(productId, quantity);
             totalPrice = totalPrice.add(productTotal);
         }
-
         return totalPrice;
     }
 }
-*/
