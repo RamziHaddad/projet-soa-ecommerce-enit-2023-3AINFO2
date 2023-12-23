106ea7e174b0a2d69d7b4ed5f836a928b7855573
rania chakroun <rania.chakroun@etudiant-enit.utm.tn>
2023-12-23 01:26:18 +0100

    test

--- catalog/src/main/java/Java/enit/Catalog/api/Product.java
+++ catalog/src/main/java/Java/enit/Catalog/api/Product.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import lombok.AllArgsConstructor;
+import Java.enit.Catalog.catalogKafkaProducer.ProductEvent;
 import Java.enit.Catalog.dto.CategoryRequest;
 import Java.enit.Catalog.dto.CategoryResponse;
 import Java.enit.Catalog.dto.ProductRequest;
@@ -20,19 +22,25 @@ import Java.enit.Catalog.dto.ProductResponse;
 import Java.enit.Catalog.service.ProductService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+
 @RestController
 @AllArgsConstructor
-@Api(tags = "Catalog API", value = "Catalog API")
+@Api(tags = "Product API", value = "Product API")
 @RequestMapping("/api/product")
 public class Product {
- private ProductService productService; 
-
- @PostMapping
- @ResponseStatus(HttpStatus.CREATED)
- @ApiOperation(value = "Créer un nouveau produit", response = Product.class)
- public Java.enit.Catalog.domain.Product createProduct(@RequestBody ProductRequest productRequest) {
-	 return productService.createProduct(productRequest);
- }
+ 	private ProductService productService; 
+    private final KafkaTemplate<String, ProductEvent> kafkaTemplate;
+ 
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	@ApiOperation(value = "Créer un nouveau produit", response = Product.class)
+ 	public Java.enit.Catalog.domain.Product createProduct(@RequestBody ProductRequest productRequest) {
+	Java.enit.Catalog.domain.Product newProduct= productService.createProduct(productRequest);
+	ProductEvent productEvent = new ProductEvent();
+    productEvent.setProduct_id(newProduct.getId());  
+    kafkaTemplate.send("catalog_topic", String.valueOf(productEvent.getProduct_id()), productEvent);
+	return newProduct; 
+	}
  @GetMapping
  @ResponseStatus(HttpStatus.OK)
  @ApiOperation(value = "Récupérer tous les produits", response = ProductResponse.class, responseContainer = "List")
